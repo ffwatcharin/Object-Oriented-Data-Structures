e7e322a81b64f2f803fe219ee1f341004e036e7b
First <watcharin9581@gmail.com>
2023-08-15 02:43:01 +0700

    yayayyy

---
+++ Chapter_Recursion/Palindrome.py
@@ -0,0 +1,16 @@
+# def ispalindrome(word):
+#     return word == word[::-1]
+
+def ispalindrome(word):
+    if len(word) < 2:
+        return True
+    if word[0] != word[-1]:
+        return False
+    return ispalindrome(word[1:-1])
+
+
+inp = input('Enter Input : ')
+if ispalindrome(inp):
+    print(f"'{inp}' is palindrome")
+else:
+    print(f"'{inp}' is not palindrome")
