976f309f15b9bd2d699fd443bd12a4927241d6e5
First <watcharin9581@gmail.com>
2023-08-15 03:25:45 +0700

    yak jung

---
+++ Chapter_Recursion/print N.py
@@ -0,0 +1,22 @@
+def print1ToN(n):
+    if n < 0:
+        return print('1 ', end='')
+    elif n >= 1:
+        print1ToN(n - 1)
+        print(n, end=' ')
+
+def printNto1(n):
+    if n < 0:
+        return print('1', end='')
+    elif n >= 1:
+        print(n, end=' ')
+        printNto1(n - 1)
+
+
+n = int(input("Enter Input : "))
+
+if n > 0 :
+    print1ToN(n)
+    printNto1(n)
+else:
+    print('1 1')
