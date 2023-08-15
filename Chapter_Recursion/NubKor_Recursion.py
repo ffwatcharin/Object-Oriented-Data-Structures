4b6ea8cc605071ebe3451ce55be209136f98869c
First <watcharin9581@gmail.com>
2023-08-15 22:19:44 +0700

    yay

--- Chapter_Recursion/NubKor_Recursion.py
+++ Chapter_Recursion/NubKor_Recursion.py
@@ -2,13 +2,11 @@ def deleteIsland(Map, y, x):
     if y < 0 or y >= len(Map) or x < 0 or x >= len(Map[0]) or Map[y][x] != 1:
         return
     Map[y][x] = 0
-    
+
     deleteIsland(Map, y - 1, x)
     deleteIsland(Map, y + 1, x)
     deleteIsland(Map, y, x - 1)
     deleteIsland(Map, y, x + 1)
-
-
 
 
 def countIsland(Map):
@@ -19,7 +17,6 @@ def countIsland(Map):
                 count += 1
                 deleteIsland(Map, y, x)
     return count
-
 
 
 inp = input("Enter Input : ").split('/')
